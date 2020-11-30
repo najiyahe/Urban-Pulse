03f0c8d8b0bd3ddcc4adfabb5b7753eeb010f8dc
najiyahe <najiyah.edun@gmail.com>
2020-11-29 23:30:37 -0500

    updated app web address

--- urban-pulse/collective-wifi/collective-wifi.ino
+++ urban-pulse/collective-wifi/collective-wifi.ino
@@ -28,7 +28,7 @@ using namespace websockets;
 
 const char ssid[] = "Fios-WE1WY"; // your network SSID (name)
 const char pass[] = "9326cycle9048tanks"; // your network password
-const char serverUrl[] = "wss://fierce-hollows-70179.herokuapp.com"; // The server you want to connect to
+const char serverUrl[] = "wss://lit-citadel-05208.herokuapp.com"; // The server you want to connect to
 
 
 
